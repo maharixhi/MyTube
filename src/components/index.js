469afe5e081e4ee5a3e69350b63b751422de6613
maharixhi <maharishi25cs075@satiengg.in>
2024-01-12 06:58:14 +0530

    done
    done

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Navbar } from './Navbar'
+export { default as Feed } from './Feed'
+export { default as ChannelDetail } from './ChannelDetail'
+export { default as SearchDetail } from './SearchDetail'
+export { default as VideoDetail } from './VideoDetail'
+export { default as Searchbar } from './SearchBar'
+export { default as Sidebar } from './SideBar'
+export { default as Videos } from './Videos'
